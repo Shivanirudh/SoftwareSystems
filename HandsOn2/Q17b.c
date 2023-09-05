1a4e09a3726d26198f0da4e09b9bebcef4b4f2ce
Shivanirudh <shivanirudh2001@gmail.com>
2023-09-05 19:12:39 +0530

    HandsOn2 17-18

---
+++ HandsOn2/Q17b.c
@@ -0,0 +1,35 @@
+/*Write a program to execute ls -l | wc.
+a. use dup
+b. use dup2
+c. use fcntl*/
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include<fcntl.h>
+#include<string.h>
+#include<sys/time.h>
+
+int main(){
+	int fd[2];
+	char buf[1024];
+	pipe(fd);
+	int cid = fork();
+	if(!cid){
+		close(fd[0]);
+		close(1);
+		dup2(fd[1], 1);
+		execl("/usr/bin/ls", "ls", "-l", (char*) NULL);
+	}
+	else{
+		close(fd[1]);
+		close(0);
+		dup2(fd[0], 0);
+		execl("/usr/bin/wc", "wc", (char*) NULL);
+	}
+	
+	return 0;
+}
+
---
+++ HandsOn2/Q18.c
@@ -0,0 +1,44 @@
+/*Write a program to find out total number of directories on the pwd.
+execute ls -l | grep ^d | wc ? Use only dup2.*/
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include<fcntl.h>
+#include<string.h>
+#include<sys/time.h>
+
+int main(){
+	int fd1[2], fd2[2];
+	char buf[1024];
+	pipe(fd1);pipe(fd2);
+	int cid = fork();
+	if(!cid){
+		close(fd2[0]);close(fd2[1]);
+		close(fd1[0]);
+		close(1);
+		dup2(fd1[1], 1);
+		execl("/usr/bin/ls", "ls", "-l", "../",(char*) NULL);
+	}
+	else{
+		int cid2 = fork();
+		if(!cid2){
+			close(fd1[1]);close(fd2[0]);
+			close(0);
+			dup2(fd1[0], 0);
+			close(1);
+			dup2(fd2[1], 1);
+			execl("/usr/bin/grep", "grep", "^d", (char*) NULL);
+		}
+		else{	
+			close(fd1[0]);close(fd1[1]);
+			close(fd2[1]);
+			close(0);
+			dup2(fd2[0], 0);
+			execl("/usr/bin/wc", "wc", (char*) NULL);
+		}
+	}
+	return 0;
+}
