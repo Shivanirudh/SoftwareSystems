599d0ebe4186465c9c6bb5cfc802901c58a47145
Shivanirudh <shivanirudh2001@gmail.com>
2023-09-05 19:19:44 +0530

    20_v2

--- HandsOn/Q20.c
+++ HandsOn/Q20.c
@@ -7,22 +7,18 @@
 #include<fcntl.h>
 #include<string.h>
 #include<sys/time.h>
+#include<sys/resource.h>
 
 int main(){
-	struct timeval start, end;
-	
-	double elapsed = 0.0;
-	
-	gettimeofday(&start, NULL);
 	int pid = getpid();
-	gettimeofday(&end, NULL);
+	int niceVal = getpriority(PRIO_PROCESS, 0);
 	
-	printf("\nProcess ID: %d", pid);
-	elapsed += (end.tv_sec - start.tv_sec)*1000000;
-	elapsed += (end.tv_usec -start.tv_usec);
-	printf("\nTime taken for getpid system call: %lf us. \n", elapsed);
-	getchar();
+	printf("Priority of process with PID %d is %d\n", pid, niceVal);
 	
+	nice(10);
+	niceVal = getpriority(PRIO_PROCESS, 0);
+	
+	printf("After modification, priority of process with PID %d is %d\n", pid, niceVal);
 	return 0;
 }
 
